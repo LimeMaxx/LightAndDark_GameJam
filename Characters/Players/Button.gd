5c1da244e8f3ba6bec8ccaa7f5538c09924ed5cb
Fizbo42 <logzilla619@gmail.com>
2024-08-15 00:07:12 -0400

    broken version

--- Characters/Players/Button.gd
+++ Characters/Players/Button.gd
@@ -1,14 +1,4 @@
 extends Button
 
-var camera2d
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	camera2d = get_node("Camera2D")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_pressed():
-	camera2d._set_position(position)
+	pass
