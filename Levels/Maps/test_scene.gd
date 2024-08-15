656320f317e319d588a5db3f6a6effc46224d6d0
Fizbo42 <logzilla619@gmail.com>
2024-08-15 19:40:17 -0400

    PathFinging
    HOLY FUCKING SHIT FINALLY, FUUUUUUCCCKKK THAT NEVER AGIAN PLEASE

--- Levels/Maps/test_scene.gd
+++ Levels/Maps/test_scene.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+var reverse
+@onready var progress = %EnemyPath.progress_ratio
+
 func _process(_delta):
-	#child_node = 
-	pass
+	if progress < 1 and reverse == 0:
+		progress += .01
+		%EnemyPath.progress_ratio = progress
+	else:
+		reverse = 1
+	if reverse == 1 and progress > 0:
+		progress -= .01
+		%EnemyPath.progress_ratio = progress
+	else:
+		reverse = 0
