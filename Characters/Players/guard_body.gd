96cc7fdb20b4936444b9c56604ec918232af6e70
Fizbo42 <logzilla619@gmail.com>
2024-08-15 21:09:04 -0400

    Movement
    fixing directions and no more moving platforms

--- Characters/Players/guard_body.gd
+++ Characters/Players/guard_body.gd
@@ -2,9 +2,29 @@ extends CharacterBody2D
 
 @export var speed = 5000
 var pause = true
+var left = Vector2(-1,0)
+var right = Vector2(1,0)
+var up = Vector2(0,-1)
+var down = Vector2(0,1)
 
 func get_input(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	if input_direction.x > 0 and input_direction.y > 0:
+		rotation_degrees = 135
+	elif input_direction.x < 0 and input_direction.y < 0:
+		rotation_degrees = 315
+	elif input_direction.x < 0 and input_direction.y > 0:
+		rotation_degrees = 225
+	elif input_direction.x > 0 and input_direction.y < 0:
+		rotation_degrees = 45
+	elif input_direction == left: 
+		rotation_degrees = 270
+	elif input_direction == right:
+		rotation_degrees = 90
+	elif input_direction == down:
+		rotation_degrees = 180
+	elif input_direction == up:
+		rotation_degrees = 0
 	velocity = input_direction * speed * delta
 
 func _physics_process(delta):
